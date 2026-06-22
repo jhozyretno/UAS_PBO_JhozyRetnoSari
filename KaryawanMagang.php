2a4f0ab794a9573a205e8f88c9b097e99aacca5a
jhozyretnosari <jhozyretno@gmail.com>
2026-06-22 14:04:14 +0700

    [Tahap 4] Implementasi Pewarisan

--- KaryawanMagang.php
+++ KaryawanMagang.php
@@ -2,32 +2,22 @@
 require_once 'Karyawan.php';
 
 class KaryawanMagang extends Karyawan {
+    // Properti khusus Karyawan Magang
     protected $uangSakuBulanan;
     protected $sertifikatKampusMerdeka;
 
     public function __construct($id_karyawan, $nama_karyawan, $departemen, $hariKerjaMasuk, $gajiDasarPerHari, $uangSakuBulanan, $sertifikatKampusMerdeka) {
-        // Memanggil constructor dari class induk (Karyawan)
+        // Pewarisan: Memanggil constructor milik induk
         parent::__construct($id_karyawan, $nama_karyawan, $departemen, $hariKerjaMasuk, $gajiDasarPerHari);
         
-        // Properti khusus Karyawan Magang
         $this->uangSakuBulanan = $uangSakuBulanan;
         $this->sertifikatKampusMerdeka = $sertifikatKampusMerdeka;
     }
 
     public function hitungGajiBersih() {
-        // Karyawan magang gajinya flat (hanya uang saku bulanan)
-        return $this->uangSakuBulanan;
     }
 
     public function tampilkanProfilKaryawan() {
-        $statusKampusMerdeka = $this->sertifikatKampusMerdeka ? "Ya" : "Tidak";
-        
-        echo "<b>[ KARYAWAN MAGANG ]</b><br>";
-        echo "ID Karyawan: {$this->id_karyawan}<br>";
-        echo "Nama: {$this->nama_karyawan}<br>";
-        echo "Departemen: {$this->departemen}<br>";
-        echo "Peserta Kampus Merdeka: {$statusKampusMerdeka}<br>";
-        echo "Gaji Bersih (Uang Saku): Rp " . number_format($this->hitungGajiBersih(), 0, ',', '.') . "<br><hr>";
     }
 }
 ?>
--- KaryawanTetap.php
+++ KaryawanTetap.php
@@ -2,30 +2,22 @@
 require_once 'Karyawan.php';
 
 class KaryawanTetap extends Karyawan {
+    // Properti khusus Karyawan Tetap
     protected $tunjanganKesehatan;
     protected $opsiSahamId;
 
     public function __construct($id_karyawan, $nama_karyawan, $departemen, $hariKerjaMasuk, $gajiDasarPerHari, $tunjanganKesehatan, $opsiSahamId) {
-        // Memanggil constructor dari class induk (Karyawan)
+        // Pewarisan: Memanggil constructor milik induk
         parent::__construct($id_karyawan, $nama_karyawan, $departemen, $hariKerjaMasuk, $gajiDasarPerHari);
         
-        // Properti khusus Karyawan Tetap
         $this->tunjanganKesehatan = $tunjanganKesehatan;
         $this->opsiSahamId = $opsiSahamId;
     }
 
     public function hitungGajiBersih() {
-        // (Hari kerja x Gaji harian) + Tunjangan Kesehatan
-        return ($this->hariKerjaMasuk * $this->gajiDasarPerHari) + $this->tunjanganKesehatan;
     }
 
     public function tampilkanProfilKaryawan() {
-        echo "<b>[ KARYAWAN TETAP ]</b><br>";
-        echo "ID Karyawan: {$this->id_karyawan}<br>";
-        echo "Nama: {$this->nama_karyawan}<br>";
-        echo "Departemen: {$this->departemen}<br>";
-        echo "ID Opsi Saham: {$this->opsiSahamId}<br>";
-        echo "Gaji Bersih: Rp " . number_format($this->hitungGajiBersih(), 0, ',', '.') . "<br><hr>";
     }
 }
 ?>
